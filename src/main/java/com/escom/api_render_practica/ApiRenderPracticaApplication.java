468e2a795de93730ad7b9cbc2b83ecd7f3573202
Wilfrido Merlin <willy2019031000merlin@gmail.com>
2026-06-23 17:02:17 -0600

    CorreccionErrores

--- src/main/java/com/escom/api_render_practica/ApiRenderPracticaApplication.java
+++ src/main/java/com/escom/api_render_practica/ApiRenderPracticaApplication.java
@@ -1,11 +1,20 @@
 package com.escom.api_render_practica;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
-public class TestController {
+public class ApiRenderPracticaApplication {
 
+	// Este es el método crucial que le faltaba a Maven para arrancar la API
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRenderPracticaApplication.class, args);
+	}
+
+	// Aquí están tus endpoints
 	@GetMapping("/")
 	public String home() {
 		return "¡Hola! La API está funcionando correctamente en la raíz.";
